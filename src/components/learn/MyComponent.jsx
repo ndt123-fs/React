8b052fde31619352e807d52237c797eff37056bc
Duy Tin <nduytin13112005@gmail.com>
2025-10-21 13:51:22 +0700

    video 32

--- src/components/learn/MyComponent.jsx
+++ src/components/learn/MyComponent.jsx
@@ -1,5 +1,22 @@
-const MyComponent = () => {
 
-    return (<div>Hello</div>)
+import "./style.css"
+
+const MyComponent = () => {
+    const a = 123;
+    const b = "Itech";
+    const user = {
+        "name": "Duy Tin",
+        "age": "20"
+    }
+    return (
+        <>
+            <div className="parent">Hello {a} {b} </div>
+            <div className="duytin">Hello {user.name} {user.age} </div>
+            <div className="duytinString">Hello {JSON.stringify(user)}</div>
+            <div className="child" style={{ borderRadius: "10px" }}>Hello</div>
+        </>
+
+    );
+
 }
 export default MyComponent;
