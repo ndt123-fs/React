013f6b1985dc17004baaf02f1b924c43a9ac2934
Duy Tin <nduytin13112005@gmail.com>
2025-10-23 19:04:06 +0700

    video 41

--- src/App.jsx
+++ src/App.jsx
@@ -6,12 +6,7 @@ import { useFormState } from "react-dom"
 import { useState } from "react"
 
 const App = () => {
-  // const name = "Eric";
-  // const age = 25;
-  // const data = {
-  //   address: "Ho Chi Minh",
-  //   country: "Viet Nam"
-  // }
+
   const [todoList, setToDoList] = useState([
     //  { id: 1, name: "Learning English" }, { id: 2, name: "Watching youtube" }
   ])
@@ -37,16 +32,14 @@ const App = () => {
       <ToDoNew
         addNewToDo={addNewToDo}
       />
-      <ToDoData
-        // name={name}
-        // age={age}
-        // data={data}
-        todoList={todoList}
-
-      />
-      <div className="todo-image">
-        <img src={logo} className="logo" />
-      </div>
+      {todoList.length > 0 ?
+        <ToDoData
+          todoList={todoList}
+        />
+        :
+        <div className="todo-image">
+          <img src={logo} className="logo" />
+        </div>}
     </div>
 
 
