31f859232562651900b60ef0b73d2ef218a497ae
Duy Tin <nduytin13112005@gmail.com>
2025-10-27 00:26:39 +0700

    xu ly not found

--- src/components/layout/header.jsx
+++ src/components/layout/header.jsx
@@ -6,7 +6,7 @@ const Header = () => {
         <ul>
             <li><NavLink class="active" to="/">Home</NavLink></li>
             <li><NavLink to="/users">User</NavLink></li>
-            <li><NavLink to="/products">Product</NavLink></li>
+            <li><NavLink to="/book">Book</NavLink></li>
 
         </ul>
 
--- src/main.jsx
+++ src/main.jsx
@@ -8,13 +8,15 @@ import {
 import LoginPage from './pages/login.jsx';
 import RegisterPage from './pages/register.jsx';
 import UserPage from './pages/user.jsx';
-import ProductPage from './pages/products.jsx';
+import BookPage from './pages/book.jsx';
 import './styles/global.css'
 import ToDoApp from './components/todo/ToDoApp.jsx';
+import ErrorPage from './pages/error.jsx';
 const router = createBrowserRouter([
   {
     path: "/",
     element: <App />,
+    errorElement: < ErrorPage />,
     children: [
 
       {
@@ -28,8 +30,8 @@ const router = createBrowserRouter([
         element: <UserPage />
       },
       {
-        path: "products",
-        element: <ProductPage />
+        path: "book",
+        element: <BookPage />
       }
     ]
   },
