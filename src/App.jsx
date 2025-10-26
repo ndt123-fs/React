73931241bc02cc8394dc4477828a9c246de555cb
Duy Tin <nduytin13112005@gmail.com>
2025-10-27 00:09:01 +0700

    index router

--- src/App.jsx
+++ src/App.jsx
@@ -1,59 +1,12 @@
-import "./components/todo/todo.css"
-import ToDoData from "./components/todo/ToDoData"
-import ToDoNew from "./components/todo/ToDoNew"
-import logo from "./assets/react.svg"
-import { useState } from "react"
+
 import Header from "./components/layout/header"
 import Footer from "./components/layout/footer"
 import { Outlet } from "react-router-dom"
 
 const App = () => {
-
-  const [todoList, setToDoList] = useState([
-    //  { id: 1, name: "Learning English" }, { id: 2, name: "Watching youtube" }
-  ])
-  const addNewToDo = (name) => {
-    const newToDo = {
-      id: randomIntFromInterval(1, 10000),
-      name: name
-    }
-    setToDoList([...todoList, newToDo]);
-  }
-  const deleteToDo = (id) => {
-    //loc bot nhung item trong list co id khac voi id truyen vao 
-    // giu lai cac item co id khac voi id ban truyen vao 
-    const newToDo = todoList.filter(item => item.id !== id);
-    console.log(newToDo)
-    setToDoList(newToDo)
-
-
-
-  }
-
-  const randomIntFromInterval = (min, max) => {
-    return Math.floor(Math.random() * (max - min - 1) + min);
-
-
-
-  }
   return (
     <>
       <Header />
-      < div className="todo-container">
-        <div className="todo-title">Todo List</div>
-        <ToDoNew
-          addNewToDo={addNewToDo}
-        />
-        {todoList.length > 0 ?
-          <ToDoData
-            todoList={todoList}
-            deleteToDo={deleteToDo}
-          />
-          :
-          <div className="todo-image">
-            <img src={logo} className="logo" />
-          </div>}
-      </div>
       <Outlet />
       <Footer />
     </>
---
+++ src/components/todo/ToDoApp.jsx
@@ -0,0 +1,57 @@
+import "./todo.css"
+import ToDoData from "./ToDoData"
+import ToDoNew from "./ToDoNew"
+import logo from "../../assets/react.svg"
+
+import { useState } from "react"
+
+const ToDoApp = () => {
+
+    const [todoList, setToDoList] = useState([
+        //  { id: 1, name: "Learning English" }, { id: 2, name: "Watching youtube" }
+    ])
+    const addNewToDo = (name) => {
+        const newToDo = {
+            id: randomIntFromInterval(1, 10000),
+            name: name
+        }
+        setToDoList([...todoList, newToDo]);
+    }
+    const deleteToDo = (id) => {
+        //loc bot nhung item trong list co id khac voi id truyen vao 
+        // giu lai cac item co id khac voi id ban truyen vao 
+        const newToDo = todoList.filter(item => item.id !== id);
+        console.log(newToDo)
+        setToDoList(newToDo)
+
+
+
+    }
+
+    const randomIntFromInterval = (min, max) => {
+        return Math.floor(Math.random() * (max - min - 1) + min);
+
+
+
+    }
+    return (
+        < div className="todo-container">
+            <div className="todo-title">Todo List</div>
+            <ToDoNew
+                addNewToDo={addNewToDo}
+            />
+            {todoList.length > 0 ?
+                <ToDoData
+                    todoList={todoList}
+                    deleteToDo={deleteToDo}
+                />
+                :
+                <div className="todo-image">
+                    <img src={logo} className="logo" />
+                </div>}
+        </div>
+    )
+
+
+}
+export default ToDoApp
